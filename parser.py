4fafc2390baf4ef8c01a7d79ba686bbb6093a92f
Clayton Daley <clayton.daley@gmail.com>
2016-02-05 10:53:13 -0600

    - update UI for larger content

--- parser.py
+++ parser.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
-from jinja2 import Template, Environment, meta
-from random import choice
 import json
+from flask import Flask, render_template, request
+from jinja2 import Environment, meta
+
 # For dynamic loading of filters
 import imp
 from inspect import getmembers, isfunction
@@ -11,44 +11,45 @@ import os
 app = Flask(__name__)
 
 # Load filters in filters dir
-filter_path='filters'
-filter_files =  [ ]
+filter_path = 'filters'
+filter_files = []
 added_filters = {}
 
 # Find py files and turn then into filterpath/blah/filter.py
 for e in os.walk(filter_path, followlinks=True):
-  for f in e[2]:
-    if f.endswith('py'):
-      print("Adding %s" % os.path.join(e[0], f))
-      filter_files.append(os.path.join(e[0], f))
+    for f in e[2]:
+        if f.endswith('py'):
+            print("Adding %s" % os.path.join(e[0], f))
+            filter_files.append(os.path.join(e[0], f))
 
 for filter in filter_files:
-    mod_name,file_ext = os.path.splitext(os.path.split(filter)[-1])
+    mod_name, file_ext = os.path.splitext(os.path.split(filter)[-1])
     py_mod = imp.load_source(mod_name, filter)
     for name, function in getmembers(py_mod):
-            if isfunction(function) and not name.startswith('_'):
-                # Saving filter info to put it in HTML at some point
-                added_filters[name] = function.__doc__
-                # add filter to jinja
-                app.jinja_env.filters[name] = function
+        if isfunction(function) and not name.startswith('_'):
+            # Saving filter info to put it in HTML at some point
+            added_filters[name] = function.__doc__
+            # add filter to jinja
+            app.jinja_env.filters[name] = function
+
 
 # These are the added filters.  must add these name + doc strings to the html
 # Also do this for built-in jinja filters
-#for f in sorted(added_filters):
+# for f in sorted(added_filters):
 #    print("%s: %s" % (f, added_filters[f]))
 
 @app.route("/")
 def hello():
     return render_template('index.html',
-                      all_filters = app.jinja_env.filters
-           )
+                           all_filters=app.jinja_env.filters
+                           )
 
 
 @app.route('/convert', methods=['GET', 'POST'])
 def convert():
-    dummy_values = [ 'Lorem', 'Ipsum', 'Amet', 'Elit', 'Expositum', 
-        'Dissimile', 'Superiori', 'Laboro', 'Torquate', 'sunt', 
-    ]
+    dummy_values = ['Lorem', 'Ipsum', 'Amet', 'Elit', 'Expositum',
+                    'Dissimile', 'Superiori', 'Laboro', 'Torquate', 'sunt',
+                    ]
 
     tpl = app.jinja_env.from_string(request.form['template'])
     values = {}
@@ -71,6 +72,7 @@ def convert():
 
     return rendered_tpl.replace('\n', '<br />')
 
+
 if __name__ == "__main__":
     app.debug = True
-    app.run(host= '0.0.0.0')
+    app.run(host='0.0.0.0')
